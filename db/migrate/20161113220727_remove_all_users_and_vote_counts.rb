e1f205833fd076133fb8d03dd0ef8a39421474d0
Undo1 <parker@erwaysoftware.com>
2016-11-14 11:20:17 -0700

    Faster

--- db/migrate/20161113220727_remove_all_users_and_vote_counts.rb
+++ db/migrate/20161113220727_remove_all_users_and_vote_counts.rb
@@ -1,6 +1,6 @@
 class RemoveAllUsersAndVoteCounts < ActiveRecord::Migration[5.0]
   def change
-    VoteCount.destroy_all
-    User.destroy_all
+    VoteCount.delete_all
+    User.delete_all
   end
 end
