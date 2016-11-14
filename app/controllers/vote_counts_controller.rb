8ceaf1e7b9f81f6bcc74e513b0822a2cd48e6722
Undo1 <parker@erwaysoftware.com>
2016-11-14 14:59:43 -0700

    What could possibly go wrong

--- app/controllers/vote_counts_controller.rb
+++ app/controllers/vote_counts_controller.rb
@@ -2,6 +2,8 @@ class VoteCountsController < ApplicationController
   def dashboard
     @users = User.order('current_score DESC')
   end
+  def graph
+  end
   def update
     users = User.select('id, current_score').order('current_score DESC')
     hash = Hash[users.map {|user| [user.id, user.current_score]}]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "vote_counts#dashboard"
+  get 'vote_counts/graph'
 end
