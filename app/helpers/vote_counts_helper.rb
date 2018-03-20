d24482babaa354d9e150524503d3b2dcbc2d6f9a
Undo1 <parker@erwaysoftware.com>
2018-03-19 21:00:49 -0600

    Halve the polling frequency

--- app/helpers/vote_counts_helper.rb
+++ app/helpers/vote_counts_helper.rb
@@ -4,10 +4,10 @@ module VoteCountsHelper
   ActiveRecord::Import.require_adapter('sqlite')
 
   def self.run_cycle
-    12.times do
+    6.times do
       result = VoteCountsHelper.fetch_votes
       break if !result
-      sleep 5
+      sleep 10
     end
   end
 
